27d6323706766dabc67bda6c32479c32d94f9ba5
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-02 12:19:21 -0500

    utilise errorhandling middleware in productController.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorhandler");
 
 
 
@@ -29,10 +30,7 @@ exports.getProductDetails = async (req, res, next) => {
 
   // Takes care if product not found 
   if (!product) {
-    return res.status(500).json({
-      success:false,
-      message: "Product not found"
-    })
+    return next(new ErrorHandler("Product Not Found", 404));
   }
 
   res.status(200).json({
@@ -46,10 +44,7 @@ exports.updateProduct = async (req, res, next) => {
   let product = await Product.findById(req.params.id);
 
   if (!product) {
-    return res.status(500).json({
-      success:false,
-      message: "Product not found"
-    })
+    return next(new ErrorHandler("Product Not Found", 404));
   }
 
   product = await Product.findByIdAndUpdate(req.params.id,req.body,{
@@ -70,10 +65,7 @@ exports.deleteProduct = async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
-    return res.status(500).json({
-      success:false,
-      message: "Product not found"
-    })
+    return next(new ErrorHandler("Product Not Found", 404));
   }
 
   await product.remove();
