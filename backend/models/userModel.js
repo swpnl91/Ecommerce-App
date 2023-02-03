fa7c823d290628dc1ad06f81f15bfe480488e1a0
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-03 15:00:11 -0500

    create user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please Enter Your Name"],
+    maxLength: [30, "Name cannot exceed 30 characters"],
+    minLength: [4, "Name should have more than 4 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please Enter Your Email"],
+    unique: true,
+    validate: [validator.isEmail, "Please Enter a valid Email"],   // validator package basically helps in validating the email
+  },
+  password: {
+    type: String,
+    required: [true, "Please Enter Your Password"],
+    minLength: [8, "Password should be greater than 8 characters"],
+    select: false,      // 'select: false' ensures that this field is not returned when the admin is trying to seek information regarding the users
+  },
+  avatar: {        // is an object (unlike array for images of products because a user has only 1 avatar )
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  role: {     // To identify whether a user is an admin or a customer
+    type: String,
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+});
+
+
+module.exports = mongoose.model("User", userSchema);
