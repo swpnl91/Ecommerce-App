01ff4b78f6ca32ceaf3ad72c3f7e61a151b9cbc9
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-05 10:36:27 -0500

    add route for changing user password

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserDetails } = require("../controllers/userController");
+const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserDetails, updatePassword } = require("../controllers/userController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 
@@ -15,6 +15,8 @@ router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").get(logout);
 
 router.route("/me").get(isAuthenticatedUser, getUserDetails);
+
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 
 
 
