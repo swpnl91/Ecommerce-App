5023d22c5342c8adf9206a1acfa56cfb3d5fab28
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-05 10:13:17 -0500

    add error handling for wrong JSON Web Token (JWT)

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -16,6 +16,12 @@ module.exports = (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
+  // Wrong JWT error
+  if (err.name === "JsonWebTokenError") {
+    const message = `Json Web Token is invalid. Try again! `;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message
