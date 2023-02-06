f9a38d3a8369d92518912e1861be79c97ff904ff
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-06 10:39:42 -0500

    add functionality for Getting a Single Order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -32,3 +32,20 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
     order,
   });
 });
+
+// Getting a Single Order
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(    // '.populate()' method basically gets you 'user', 'name', 'email' fields by checking in the 'user' database as the 'user' field in the 'order' database only includes its 'id' since that's how we establish relations in Mongodb 
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(new ErrorHander("Order not found with this Id", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
