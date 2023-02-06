3b5c1d8ba843baf98d97895579751ddea74295fd
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-06 10:44:52 -0500

    add a route for Getting the list of Orders for a logged in user

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { newOrder, getSingleOrder } = require("../controllers/orderController");
+const { newOrder, getSingleOrder, myOrders } = require("../controllers/orderController");
 
 const router = express.Router();
 
@@ -9,7 +9,7 @@ router.route("/order/new").post(isAuthenticatedUser, newOrder);
 
 router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 
-
+router.route("/orders/me").get(isAuthenticatedUser, myOrders);
 
 
 module.exports = router;
