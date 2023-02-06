ac791f313d7f6bf57b1161609a26ac4a8e1a2334
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-06 10:25:25 -0500

    add route for creating a new order

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const { newOrder } = require("../controllers/orderController");
 
 const router = express.Router();
 
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
+router.route("/order/new").post(isAuthenticatedUser, newOrder);
 
 
 
