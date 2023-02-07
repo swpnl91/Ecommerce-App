563a1ab3d02e561ce27dd1ab9bd740463a2f5f1d
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-07 13:15:56 -0500

    add rightfooter footer.js

--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -7,6 +7,7 @@ import appStore from "../../../images/Appstore.png";
 const Footer = () => {
   return (
     <footer id="footer">
+
       <div className="leftFooter">
         <h4>DOWNLOAD OUR APP</h4>
         <p>Download App for Android and IOS mobile phone</p>
@@ -20,6 +21,14 @@ const Footer = () => {
 
         <p>&copy; 2023; JhumriTalaiya, Inc. All Rights Reserved.</p>
       </div>
+
+      <div className="rightFooter">
+        <h4>Follow Us On</h4>
+        <a href="http://instagram.com">Instagram</a>
+        <a href="http://youtube.com">Youtube</a>
+        <a href="http://instagram.com">Facebook</a>
+      </div>
+
     </footer>
   );
 };
