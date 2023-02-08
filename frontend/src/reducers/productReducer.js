5724bb2a96d6a64fafcb478fd3f233e90e0fc298
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-08 15:21:41 -0500

    add product reducer in store.js

--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -2,7 +2,7 @@ import {
   ALL_PRODUCT_FAIL,
   ALL_PRODUCT_REQUEST,
   ALL_PRODUCT_SUCCESS,
-  
+  CLEAR_ERRORS
 } from "../constants/productConstants";
 
 
@@ -27,6 +27,11 @@ export const productReducer = (state = {}, action) => {
           loading: false,
           error: action.payload
           
+        };
+      case CLEAR_ERRORS:
+        return {
+           ...state,
+          error: null,
         };
       default:
         return state;
