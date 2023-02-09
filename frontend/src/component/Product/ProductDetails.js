62305c232a755e00f85b315dff8394f7a0934a80
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-09 16:44:31 -0500

    add functionality to render product reviews in productDetails.js

--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -6,6 +6,7 @@ import {
   clearErrors,
   getProductDetails,
 } from "../../actions/productAction";
+import ReviewCard from "./ReviewCard.js";
 
 
 const ProductDetails = ({ match }) => {     /* match is received as a prop (default prop) Google 'match' object/parameter in react*/
@@ -51,49 +52,68 @@ const ProductDetails = ({ match }) => {     /* match is received as a prop (defa
             ))}
           </Carousel>
         </div>
-
-        <div className="detailsBlock-1">
-          <h2>{product.name}</h2>
-          <p>Product # {product._id}</p>
-        </div>
-
-        <div className="detailsBlock-2">
-           {/* <Rating {...options} /> */}
-          <span className="detailsBlock-2-span">
-            {" "}
-            ({product.numOfReviews} Reviews)
-          </span>
-        </div>
-
-        <div className="detailsBlock-3">
-          <h1>{`₹${product.price}`}</h1>
-          <div className="detailsBlock-3-1">
-            <div className="detailsBlock-3-1-1">
-              <button onClick={decreaseQuantity}>-</button>
-              <input readOnly type="number" value={quantity} />
-              <button onClick={increaseQuantity}>+</button>
-            </div>
-            <button
-              disabled={product.Stock < 1 ? true : false}
-              onClick={addToCartHandler}
-            >
-              Add to Cart
-            </button>
+        <div>
+          <div className="detailsBlock-1">
+            <h2>{product.name}</h2>
+            <p>Product # {product._id}</p>
           </div>
 
-          <p>
-            Status:
-            <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
-              {product.Stock < 1 ? "OutOfStock" : "InStock"}
-            </b>
-          </p>
-        </div>
+          <div className="detailsBlock-2">
+            {/* <Rating {...options} /> */}
+            <span className="detailsBlock-2-span">
+              {" "}
+              ({product.numOfReviews} Reviews)
+            </span>
+          </div>
 
-        <div className="detailsBlock-4">
-          Description : <p>{product.description}</p>
-        </div>
+          <div className="detailsBlock-3">
+            <h1>{`₹${product.price}`}</h1>
+            <div className="detailsBlock-3-1">
+              <div className="detailsBlock-3-1-1">
+                <button onClick={decreaseQuantity}>-</button>
+                <input readOnly type="number" value={quantity} />
+                <button onClick={increaseQuantity}>+</button>
+              </div>
+              <button
+                disabled={product.Stock < 1 ? true : false}
+                onClick={addToCartHandler}
+              >
+                Add to Cart
+              </button>
+            </div>
 
+            <p>
+              Status:
+              <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+                {product.Stock < 1 ? "OutOfStock" : "InStock"}
+              </b>
+            </p>
+          </div>
+
+          <div className="detailsBlock-4">
+            Description : <p>{product.description}</p>
+          </div>
+            
+          <button onClick={submitReviewToggle} className="submitReview">
+                Submit Review
+          </button>
+        </div>
       </div>
+
+      <h3 className="reviewsHeading">REVIEWS</h3>
+
+      
+      {product.reviews && product.reviews[0] ? (
+        <div className="reviews">
+          {product.reviews &&
+            product.reviews.map((review) => (
+              <ReviewCard key={review._id} review={review} />
+            ))}
+        </div>
+      ) : (
+        <p className="noReviews">No Reviews Yet</p>
+      )}
+
     </Fragment>
   );
 };
