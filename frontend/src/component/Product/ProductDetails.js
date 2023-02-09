1aae4969a1c8519b14334215896431ec06f05d1f
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-09 18:47:10 -0500

    add functionality for alert

--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -7,11 +7,14 @@ import {
   getProductDetails,
 } from "../../actions/productAction";
 import ReviewCard from "./ReviewCard.js";
+import Loader from "../layout/Loader/Loader";
+import { useAlert } from "react-alert";
 
 
 const ProductDetails = ({ match }) => {     /* match is received as a prop (default prop) Google 'match' object/parameter in react*/
   
   const dispatch = useDispatch();
+  const alert = useAlert();
 
   const { product, loading, error } = useSelector(
     (state) => state.productDetails
@@ -33,87 +36,93 @@ const ProductDetails = ({ match }) => {     /* match is received as a prop (defa
     }
 
     dispatch(getProductDetails(match.params.id));       /* we have 'req.param.id in backend in front end we have 'match.params.id' and it's received as a prop */
-  }, [dispatch, match.params.id, error]);
+  }, [dispatch, match.params.id, error, alert,]);
 
 
   return (
     <Fragment>
-      <div className="ProductDetails">
-        <div>
-          <Carousel>
-          {product.images &&
-            product.images.map((item, i) => (      /* 'i' is the index and the second argument that .map() can take */
-              <img  
-                className="CarouselImage"
-                key={i}       /* 'i' is used as 'key' which is necessary with .map() */
-                src={item.url}
-                alt={`${i} Slide`}
-              />
-            ))}
-          </Carousel>
-        </div>
-        <div>
-          <div className="detailsBlock-1">
-            <h2>{product.name}</h2>
-            <p>Product # {product._id}</p>
-          </div>
+      {loading ? <Loader /> : (
 
-          <div className="detailsBlock-2">
-            {/* <Rating {...options} /> */}
-            <span className="detailsBlock-2-span">
-              {" "}
-              ({product.numOfReviews} Reviews)
-            </span>
-          </div>
-
-          <div className="detailsBlock-3">
-            <h1>{`₹${product.price}`}</h1>
-            <div className="detailsBlock-3-1">
-              <div className="detailsBlock-3-1-1">
-                <button onClick={decreaseQuantity}>-</button>
-                <input readOnly type="number" value={quantity} />
-                <button onClick={increaseQuantity}>+</button>
+        <Fragment>
+          <div className="ProductDetails">
+            <div>
+              <Carousel>
+              {product.images &&
+                product.images.map((item, i) => (      /* 'i' is the index and the second argument that .map() can take */
+                  <img  
+                    className="CarouselImage"
+                    key={i}       /* 'i' is used as 'key' which is necessary with .map() */
+                    src={item.url}
+                    alt={`${i} Slide`}
+                  />
+                ))}
+              </Carousel>
+            </div>
+            <div>
+              <div className="detailsBlock-1">
+                <h2>{product.name}</h2>
+                <p>Product # {product._id}</p>
               </div>
-              <button
-                disabled={product.Stock < 1 ? true : false}
-                onClick={addToCartHandler}
-              >
-                Add to Cart
+
+              <div className="detailsBlock-2">
+                {/* <Rating {...options} /> */}
+                <span className="detailsBlock-2-span">
+                  {" "}
+                  ({product.numOfReviews} Reviews)
+                </span>
+              </div>
+
+              <div className="detailsBlock-3">
+                <h1>{`₹${product.price}`}</h1>
+                <div className="detailsBlock-3-1">
+                  <div className="detailsBlock-3-1-1">
+                    <button onClick={decreaseQuantity}>-</button>
+                    <input readOnly type="number" value={quantity} />
+                    <button onClick={increaseQuantity}>+</button>
+                  </div>
+                  <button
+                    disabled={product.Stock < 1 ? true : false}
+                    onClick={addToCartHandler}
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+
+                <p>
+                  Status:
+                  <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+                    {product.Stock < 1 ? "OutOfStock" : "InStock"}
+                  </b>
+                </p>
+              </div>
+
+              <div className="detailsBlock-4">
+                Description : <p>{product.description}</p>
+              </div>
+                
+              <button onClick={submitReviewToggle} className="submitReview">
+                    Submit Review
               </button>
             </div>
-
-            <p>
-              Status:
-              <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
-                {product.Stock < 1 ? "OutOfStock" : "InStock"}
-              </b>
-            </p>
           </div>
 
-          <div className="detailsBlock-4">
-            Description : <p>{product.description}</p>
-          </div>
-            
-          <button onClick={submitReviewToggle} className="submitReview">
-                Submit Review
-          </button>
-        </div>
-      </div>
-
-      <h3 className="reviewsHeading">REVIEWS</h3>
+          <h3 className="reviewsHeading">REVIEWS</h3>
 
       
-      {product.reviews && product.reviews[0] ? (
-        <div className="reviews">
-          {product.reviews &&
-            product.reviews.map((review) => (
-              <ReviewCard key={review._id} review={review} />
-            ))}
-        </div>
-      ) : (
-        <p className="noReviews">No Reviews Yet</p>
-      )}
+          {product.reviews && product.reviews[0] ? (
+            <div className="reviews">
+              {product.reviews &&
+                product.reviews.map((review) => (
+                  <ReviewCard key={review._id} review={review} />
+                ))}
+            </div>
+          ) : (
+            <p className="noReviews">No Reviews Yet</p>
+          )}
 
+        </Fragment>
+
+      )}
     </Fragment>
   );
 };
