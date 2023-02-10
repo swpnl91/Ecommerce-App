49e284f798171ef5fce0817ed308d373312395a6
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-09 19:24:42 -0500

    import essential files and create useEffect function and a basicstructure in Products.js

--- frontend/src/component/Product/Products.js
+++ frontend/src/component/Product/Products.js
@@ -8,11 +8,35 @@ import ProductCard from "../Home/ProductCard";
 
 
 const Products = ({ match }) => {
-  
+  const dispatch = useDispatch();
+
+  const alert = useAlert();
+
+  const {
+    products,
+    loading,
+    error,
+    productsCount,
+  } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+
+    dispatch(getProduct());
+  }, [dispatch, alert, error]);
 
   return (
     <Fragment>
-      
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          
+        </Fragment>
+      )}
     </Fragment>
   );
 };
