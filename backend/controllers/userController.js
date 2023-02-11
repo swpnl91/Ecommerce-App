415e3f244281dc631e3cede18e33441b96fdf1a1
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-11 15:08:50 -0500

    make changes in backend to accommodate functionality for cloudinary

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,10 +4,17 @@ const User = require("../models/userModel");
 const sendToken = require("../utils/jwtToken");
 const sendEmail = require("../utils/sendEmail");
 const crypto = require("crypto");
+const cloudinary = require("cloudinary");
 
 // Registering a User
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
   
+  const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+    folder: "avatars",  // In your cloudinary account in media library, you've to creat a folder named 'avatars' for this to work
+    width: 150,
+    crop: "scale",
+  });
+
   const { name, email, password } = req.body;
 
   const user = await User.create({
@@ -15,8 +22,8 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     email,
     password,
     avatar: {
-      public_id: "sample id",
-      url: "profilepicurl"
+      public_id: myCloud.public_id,
+      url: myCloud.secure_url
     }
   });
 
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-
+const cloudinary = require("cloudinary");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 
@@ -16,6 +16,13 @@ dotenv.config({path:"backend/config/config.env"});
 
 // Connecting to database 
 connectDatabase();
+
+// get cloudi-name, API-key and secret-key by signing up for cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is listening on http://localhost:${process.env.PORT}`)
