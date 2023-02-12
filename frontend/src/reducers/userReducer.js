8a4a87755ca0c255eba4c63746113254ee4f7027
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-12 11:24:21 -0500

    add logout-success/logout-fail cases in userReducer

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -8,6 +8,8 @@ import {
   LOAD_USER_REQUEST,
   LOAD_USER_SUCCESS,
   LOAD_USER_FAIL,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAIL,
 
   CLEAR_ERRORS
   
@@ -32,7 +34,13 @@ export const userReducer = (state = { user: {} }, action) => {
         user: action.payload,
       };
 
-  
+    case LOGOUT_SUCCESS:
+      return {
+        loading: false,
+        user: null,
+        isAuthenticated: false,
+      };
+
     case LOGIN_FAIL:
     case REGISTER_USER_FAIL:
       return {
@@ -52,7 +60,12 @@ export const userReducer = (state = { user: {} }, action) => {
         error: action.payload,
       };
 
-    
+    case LOGOUT_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
 
     case CLEAR_ERRORS:
       return {
