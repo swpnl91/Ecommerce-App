f5f1730c5bd0b49cf158c3be02fe8e9d5d701f77
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-13 13:21:20 -0500

    create UpdatePassword.js

---
+++ frontend/src/component/User/UpdatePassword.js
@@ -0,0 +1,26 @@
+import React, { Fragment, useState, useEffect } from "react";
+import "./UpdatePassword.css";
+
+
+const UpdatePassword = ({ history }) => {
+  
+
+  return (
+    <Fragment>
+      
+        <Fragment>
+          
+          <div className="updatePasswordContainer">
+            <div className="updatePasswordBox">
+              <h2 className="updatePasswordHeading">Update Profile</h2>
+
+              
+            </div>
+          </div>
+        </Fragment>
+      
+    </Fragment>
+  );
+};
+
+export default UpdatePassword;
