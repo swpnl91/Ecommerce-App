f59a50f7a1ac6e4a546b0dc8a1b5fad0655df943
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-13 12:34:02 -0500

    add structure UpdateProfile.js

--- frontend/src/component/User/UpdateProfile.js
+++ frontend/src/component/User/UpdateProfile.js
@@ -69,13 +69,65 @@ const UpdateProfile = ({ history }) => {
     }
   }, [dispatch, error, alert, history, user, isUpdated]);
 
-  
+
 
   return (
     <Fragment>
       
         <Fragment>
-          
+          <div className="updateProfileContainer">
+            <div className="updateProfileBox">
+              <h2 className="updateProfileHeading">Update Profile</h2>
+
+              <form
+                className="updateProfileForm"
+                encType="multipart/form-data"
+                onSubmit={updateProfileSubmit}
+              >
+
+                <div className="updateProfileName">
+                  <FaceIcon />
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    required
+                    name="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+
+                <div className="updateProfileEmail">
+                  <MailOutlineIcon />
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    required
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+
+                <div id="updateProfileImage">
+                  <img src={avatarPreview} alt="Avatar Preview" />
+                  <input
+                    type="file"
+                    name="avatar"
+                    accept="image/*"
+                    onChange={updateProfileDataChange}
+                  />
+                </div>
+
+                <input
+                  type="submit"
+                  value="Update"
+                  className="updateProfileBtn"
+                />
+                
+              </form>
+            </div>
+          </div>
         </Fragment>
       
     </Fragment>
