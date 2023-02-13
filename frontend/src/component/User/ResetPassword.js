863249fe92c30f3408df29a641d550c5ef23772a
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-13 15:21:44 -0500

    add basic structure for ResetPassword.js

---
+++ frontend/src/component/User/ResetPassword.js
@@ -0,0 +1,26 @@
+import React, { Fragment, useState, useEffect } from "react";
+import "./ResetPassword.css";
+
+
+const ResetPassword = ({ history, match }) => {
+  
+
+  return (
+    <Fragment>
+      
+        <Fragment>
+          
+          <div className="resetPasswordContainer">
+            <div className="resetPasswordBox">
+              <h2 className="resetPasswordHeading">Update Profile</h2>
+
+              
+            </div>
+          </div>
+        </Fragment>
+      
+    </Fragment>
+  );
+};
+
+export default ResetPassword;
