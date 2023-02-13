40a9113ff1f8b261798d6f1a295323f991fb49c6
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-13 14:46:36 -0500

    add structure and functionality for forget password in ForgotPassword.js

--- frontend/src/component/User/ForgotPassword.js
+++ frontend/src/component/User/ForgotPassword.js
@@ -18,6 +18,26 @@ const ForgotPassword = () => {
 
   const [email, setEmail] = useState("");
 
+  const forgotPasswordSubmit = (e) => {
+    e.preventDefault();
+
+    const myForm = new FormData();
+
+    myForm.set("email", email);
+    dispatch(forgotPassword(myForm));
+  };
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+
+    if (message) {
+      alert.success(message);
+    }
+  }, [dispatch, error, alert, message]);
+
   return (
     <Fragment>
       
@@ -27,6 +47,30 @@ const ForgotPassword = () => {
             <div className="forgotPasswordBox">
               <h2 className="forgotPasswordHeading">Forgot Password</h2>
 
+              <form
+                className="forgotPasswordForm"
+                onSubmit={forgotPasswordSubmit}
+              >
+
+                <div className="forgotPasswordEmail">
+                  <MailOutlineIcon />
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    required
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+
+                <input
+                  type="submit"
+                  value="Send"
+                  className="forgotPasswordBtn"
+                />
+
+              </form>
               
             </div>
           </div>
