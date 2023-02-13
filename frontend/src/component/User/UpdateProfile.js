348faab1b61d94537e66816d4ce3fcb8c95da327
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-13 11:52:45 -0500

    create UpdateProfile.js

---
+++ frontend/src/component/User/UpdateProfile.js
@@ -0,0 +1,35 @@
+import React, { Fragment, useState, useEffect } from "react";
+import "./UpdateProfile.css";
+import Loader from "../layout/Loader/Loader";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import FaceIcon from "@material-ui/icons/Face";
+import { useDispatch, useSelector } from "react-redux";
+import { clearErrors, updateProfile, loadUser } from "../../actions/userAction";
+import { useAlert } from "react-alert";
+import { UPDATE_PROFILE_RESET } from "../../constants/userConstants";
+import MetaData from "../layout/MetaData";
+
+const UpdateProfile = ({ history }) => {
+  const dispatch = useDispatch();
+  const alert = useAlert();
+
+  const { user } = useSelector((state) => state.user);
+  const { error, isUpdated, loading } = useSelector((state) => state.profile);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [avatar, setAvatar] = useState();
+  const [avatarPreview, setAvatarPreview] = useState("/Profile.png");
+  
+  return (
+    <Fragment>
+      
+        <Fragment>
+          
+        </Fragment>
+      
+    </Fragment>
+  );
+};
+
+export default UpdateProfile;
