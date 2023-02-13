2e4fa18011c3d8200fcff6208c2a5d97e09445f2
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-13 15:26:23 -0500

    add functionality and logic for ResetPassword.js

--- frontend/src/component/User/ResetPassword.js
+++ frontend/src/component/User/ResetPassword.js
@@ -11,6 +11,39 @@ import LockIcon from "@material-ui/icons/Lock";
 
 const ResetPassword = ({ history, match }) => {
   
+  const dispatch = useDispatch();
+  const alert = useAlert();
+
+  const { error, success, loading } = useSelector(
+    (state) => state.forgotPassword
+  );
+
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const resetPasswordSubmit = (e) => {
+    e.preventDefault();
+
+    const myForm = new FormData();
+
+    myForm.set("password", password);
+    myForm.set("confirmPassword", confirmPassword);
+
+    dispatch(resetPassword(match.params.token, myForm));
+  };
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+
+    if (success) {
+      alert.success("Password Updated Successfully");
+
+      history.push("/login");
+    }
+  }, [dispatch, error, alert, history, success]);
 
   return (
     <Fragment>
@@ -21,6 +54,40 @@ const ResetPassword = ({ history, match }) => {
             <div className="resetPasswordBox">
               <h2 className="resetPasswordHeading">Update Profile</h2>
 
+              <form
+                className="resetPasswordForm"
+                onSubmit={resetPasswordSubmit}
+              >
+              
+                <div>
+                  <LockOpenIcon />
+                  <input
+                    type="password"
+                    placeholder="New Password"
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+
+                <div className="loginPassword">
+                  <LockIcon />
+                  <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    required
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  />
+                </div>
+
+                <input
+                  type="submit"
+                  value="Update"
+                  className="resetPasswordBtn"
+                />
+
+              </form>
               
             </div>
           </div>
