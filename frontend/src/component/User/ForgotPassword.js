ae21195f1dfdc627f898cb87dc98581716cd1679
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-13 14:25:03 -0500

    create ForgotPassword.js

---
+++ frontend/src/component/User/ForgotPassword.js
@@ -0,0 +1,25 @@
+import React, { Fragment, useState, useEffect } from "react";
+import "./ForgotPassword.css";
+
+
+const ForgotPassword = () => {
+  
+  return (
+    <Fragment>
+      
+        <Fragment>
+          
+          <div className="forgotPasswordContainer">
+            <div className="forgotPasswordBox">
+              <h2 className="forgotPasswordHeading">Forgot Password</h2>
+
+              
+            </div>
+          </div>
+        </Fragment>
+      
+    </Fragment>
+  );
+};
+
+export default ForgotPassword;
