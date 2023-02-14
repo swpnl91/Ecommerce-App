235ce921b973362b83773b57caa8be303698fc29
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-14 14:03:32 -0500

    create CartItemCard.js

---
+++ frontend/src/component/Cart/CartItemCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./CartItemCard.css";
+import { Link } from "react-router-dom";
+
+const CartItemCard = ({ item, deleteCartItems }) => {
+  return (
+    <div className="CartItemCard">
+      
+    </div>
+  );
+};
+
+export default CartItemCard;
