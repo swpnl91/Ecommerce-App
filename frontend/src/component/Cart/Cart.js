14742f04aad647bfab2e7230f1feae59b0065b05
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-14 14:00:45 -0500

    create Cart.js

---
+++ frontend/src/component/Cart/Cart.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+import "./Cart.css";
+import CartItemCard from "./CartItemCard";
+
+
+const Cart = ({ history }) => {
+  
+  return (
+    <Fragment>
+      
+        <Fragment>
+          <div className="cartPage">
+            
+          </div>
+        </Fragment>
+      
+    </Fragment>
+  );
+};
+
+export default Cart;
