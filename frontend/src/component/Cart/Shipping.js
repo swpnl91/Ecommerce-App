30c511122906c7a96e60c0f8a8c9059a44ac7722
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-15 13:37:52 -0500

    import required files and packages to Shipping.js

--- frontend/src/component/Cart/Shipping.js
+++ frontend/src/component/Cart/Shipping.js
@@ -1,6 +1,16 @@
 import React, { Fragment, useState } from "react";
 import "./Shipping.css";
-
+import { useSelector, useDispatch } from "react-redux";
+import { saveShippingInfo } from "../../actions/cartAction";
+import MetaData from "../layout/MetaData";
+import PinDropIcon from "@material-ui/icons/PinDrop";
+import HomeIcon from "@material-ui/icons/Home";
+import LocationCityIcon from "@material-ui/icons/LocationCity";
+import PublicIcon from "@material-ui/icons/Public";
+import PhoneIcon from "@material-ui/icons/Phone";
+import TransferWithinAStationIcon from "@material-ui/icons/TransferWithinAStation";
+import { Country, State } from "country-state-city";
+import { useAlert } from "react-alert";
 
 const Shipping = ({ history }) => {
   
