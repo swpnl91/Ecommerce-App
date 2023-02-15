b8a76acf958ff8bb9a09288fe461bfe9b23258b7
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-15 15:12:52 -0500

    import files and packages for ConfirmOrder.js

--- frontend/src/component/Cart/ConfirmOrder.js
+++ frontend/src/component/Cart/ConfirmOrder.js
@@ -1,5 +1,10 @@
 import React, { Fragment } from "react";
-
+import CheckoutSteps from "../Cart/CheckoutSteps";
+import { useSelector } from "react-redux";
+import MetaData from "../layout/MetaData";
+import "./ConfirmOrder.css";
+import { Link } from "react-router-dom";
+import { Typography } from "@material-ui/core";
 
 const ConfirmOrder = ({ history }) => {
   
