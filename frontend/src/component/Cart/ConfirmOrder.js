9f9d4ac5511e32f98257d987043d9b79346936bf
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-15 15:21:21 -0500

    add structure and logic for ConfirmOrder.js

--- frontend/src/component/Cart/ConfirmOrder.js
+++ frontend/src/component/Cart/ConfirmOrder.js
@@ -34,6 +34,99 @@ const ConfirmOrder = ({ history }) => {
     
       <div className="confirmOrderPage">
         
+      <div>
+
+          <div className="confirmshippingArea">
+            <Typography>Shipping Info</Typography>
+            
+            <div className="confirmshippingAreaBox">
+              
+              <div>
+                <p>Name:</p>
+                <span>{user.name}</span>
+              </div>
+
+              <div>
+                <p>Phone:</p>
+                <span>{shippingInfo.phoneNo}</span>
+              </div>
+
+              <div>
+                <p>Address:</p>
+                <span>{address}</span>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div className="confirmCartItems">
+            
+            <Typography>Your Cart Items:</Typography>
+            
+            <div className="confirmCartItemsContainer">
+              
+              {cartItems &&
+                cartItems.map((item) => (
+                  <div key={item.product}>
+                    <img src={item.image} alt="Product" />
+                    <Link to={`/product/${item.product}`}>
+                      {item.name}
+                    </Link>{" "}
+                    <span>
+                      {item.quantity} X ₹{item.price} ={" "}
+                      <b>₹{item.price * item.quantity}</b>
+                    </span>
+                  </div>
+                ))}
+
+            </div>
+
+          </div>
+
+        </div>
+
+        {/*  */}
+
+        <div>
+
+          <div className="orderSummary">
+            
+            <Typography>Order Summery</Typography>
+            
+            <div>
+              
+              <div>
+                <p>Subtotal:</p>
+                <span>₹{subtotal}</span>
+              </div>
+              
+              <div>
+                <p>Shipping Charges:</p>
+                <span>₹{shippingCharges}</span>
+              </div>
+              
+              <div>
+                <p>GST:</p>
+                <span>₹{tax}</span>
+              
+              </div>
+            
+            </div>
+
+            <div className="orderSummaryTotal">
+              <p>
+                <b>Total:</b>
+              </p>
+              <span>₹{totalPrice}</span>
+            </div>
+
+            <button onClick={proceedToPayment}>Proceed To Payment</button>
+          
+          </div>
+
+        </div>
+
       </div>
 
     </Fragment>
