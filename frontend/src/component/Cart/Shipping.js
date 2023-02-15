4e746d5674a6beb384f8dbde52b0cea83af1b61a
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-15 13:33:00 -0500

    create basic structure for Shipping.js

---
+++ frontend/src/component/Cart/Shipping.js
@@ -0,0 +1,27 @@
+import React, { Fragment, useState } from "react";
+import "./Shipping.css";
+
+
+const Shipping = ({ history }) => {
+  
+
+  return (
+    <Fragment>
+      
+
+
+      
+
+      <div className="shippingContainer">
+        <div className="shippingBox">
+          <h2 className="shippingHeading">Shipping Details</h2>
+
+          
+        </div>
+      </div>
+
+    </Fragment>
+  );
+};
+
+export default Shipping;
