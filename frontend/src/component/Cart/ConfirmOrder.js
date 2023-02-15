87a9075c3d0de8c949283037b3e380a5f6270899
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-15 15:09:45 -0500

    add basic structure for ConfirmOrder.js

---
+++ frontend/src/component/Cart/ConfirmOrder.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+
+
+const ConfirmOrder = ({ history }) => {
+  
+
+  return (
+    <Fragment>
+
+      
+
+      <div className="confirmOrderPage">
+        
+      </div>
+
+    </Fragment>
+  );
+};
+
+export default ConfirmOrder;
