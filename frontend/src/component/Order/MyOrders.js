30e40579466bcd60e9c973db47686ae5a0690362
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-16 15:08:10 -0500

    import required files/packages in  MyOrders.js

--- frontend/src/component/Order/MyOrders.js
+++ frontend/src/component/Order/MyOrders.js
@@ -1,5 +1,14 @@
 import React, { Fragment, useEffect } from "react";
 import "./MyOrders.css";
+import { DataGrid } from "@material-ui/data-grid";
+import { useSelector, useDispatch } from "react-redux";
+import { clearErrors, myOrders } from "../../actions/orderAction";
+import Loader from "../layout/Loader/Loader";
+import { Link } from "react-router-dom";
+import { useAlert } from "react-alert";
+import Typography from "@material-ui/core/Typography";
+import MetaData from "../layout/MetaData";
+import LaunchIcon from "@material-ui/icons/Launch";
 
 
 const MyOrders = () => {
