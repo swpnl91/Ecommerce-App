d3ab06b2ec7f2521fc3863e2ceed18fb679ef718
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-16 13:36:33 -0500

    craete Payment.js

---
+++ frontend/src/component/Cart/Payment.js
@@ -0,0 +1,20 @@
+import React, { Fragment, useEffect, useRef } from "react";
+import "./Payment.css";
+
+
+
+const Payment = ({ history }) => {
+  
+
+  return (
+    <Fragment>
+      
+      <div className="paymentContainer">
+        
+      </div>
+      
+    </Fragment>
+  );
+};
+
+export default Payment;
