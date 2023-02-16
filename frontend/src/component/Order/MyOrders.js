b061cc079ad08081e65fa9e04cf4f757d1e60a7b
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-16 15:07:07 -0500

    create MyOrders.js

---
+++ frontend/src/component/Order/MyOrders.js
@@ -0,0 +1,23 @@
+import React, { Fragment, useEffect } from "react";
+import "./MyOrders.css";
+
+
+const MyOrders = () => {
+
+
+  return (
+    <Fragment>
+      
+
+      
+        <div className="myOrdersPage">
+
+          
+
+        </div>
+      
+    </Fragment>
+  );
+};
+
+export default MyOrders;
