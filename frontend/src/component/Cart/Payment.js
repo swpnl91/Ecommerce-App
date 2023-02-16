20c61413044b1f4f4dc7a0af699fef3fc7ab37e8
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-16 13:38:13 -0500

    import required files/packages in Payment.js

--- frontend/src/component/Cart/Payment.js
+++ frontend/src/component/Cart/Payment.js
@@ -1,6 +1,22 @@
 import React, { Fragment, useEffect, useRef } from "react";
 import "./Payment.css";
-
+import CheckoutSteps from "../Cart/CheckoutSteps";
+import { useSelector, useDispatch } from "react-redux";
+import MetaData from "../layout/MetaData";
+import { Typography } from "@material-ui/core";
+import { useAlert } from "react-alert";
+import {
+  CardNumberElement,
+  CardCvcElement,
+  CardExpiryElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+import axios from "axios";
+import CreditCardIcon from "@material-ui/icons/CreditCard";
+import EventIcon from "@material-ui/icons/Event";
+import VpnKeyIcon from "@material-ui/icons/VpnKey";
+import { createOrder, clearErrors } from "../../actions/orderAction";
 
 
 const Payment = ({ history }) => {
@@ -12,7 +28,7 @@ const Payment = ({ history }) => {
       <div className="paymentContainer">
         
       </div>
-      
+
     </Fragment>
   );
 };
