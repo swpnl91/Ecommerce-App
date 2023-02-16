e02da0d0068b0e99b5e09124cd9927530b68d30f
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-16 14:46:23 -0500

    import required files and packages in OrderSuccess.js

--- frontend/src/component/Cart/OrderSuccess.js
+++ frontend/src/component/Cart/OrderSuccess.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./OrderSuccess.css";
-
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import { Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 const OrderSuccess = () => {
   return (
