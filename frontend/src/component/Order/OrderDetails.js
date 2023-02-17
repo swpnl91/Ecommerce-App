df69418864a94699d1da99dc7763b48530874603
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-17 14:22:29 -0500

    add div for orderDetailsContainer in OrderDetails.js

--- frontend/src/component/Order/OrderDetails.js
+++ frontend/src/component/Order/OrderDetails.js
@@ -24,7 +24,72 @@ const OrderDetails = ({ match }) => {
             
             <div className="orderDetailsContainer">
               
+              <Typography component="h1">
+                Order #{order && order._id}
+              </Typography>
               
+              <Typography>Shipping Info</Typography>
+              
+              <div className="orderDetailsContainerBox">
+                <div>
+                  <p>Name:</p>
+                  <span>{order.user && order.user.name}</span>
+                </div>
+                <div>
+                  <p>Phone:</p>
+                  <span>
+                    {order.shippingInfo && order.shippingInfo.phoneNo}
+                  </span>
+                </div>
+                <div>
+                  <p>Address:</p>
+                  <span>
+                    {order.shippingInfo &&
+                      `${order.shippingInfo.address}, ${order.shippingInfo.city}, ${order.shippingInfo.state}, ${order.shippingInfo.pinCode}, ${order.shippingInfo.country}`}
+                  </span>
+                </div>
+              </div>
+
+              <Typography>Payment</Typography>
+              
+              <div className="orderDetailsContainerBox">
+                <div>
+                  <p
+                    className={
+                      order.paymentInfo &&
+                      order.paymentInfo.status === "succeeded"
+                        ? "greenColor"
+                        : "redColor"
+                    }
+                  >
+                    {order.paymentInfo &&
+                    order.paymentInfo.status === "succeeded"
+                      ? "PAID"
+                      : "NOT PAID"}
+                  </p>
+                </div>
+
+                <div>
+                  <p>Amount:</p>
+                  <span>{order.totalPrice && order.totalPrice}</span>
+                </div>
+              </div>
+
+              <Typography>Order Status</Typography>
+              
+              <div className="orderDetailsContainerBox">
+                <div>
+                  <p
+                    className={
+                      order.orderStatus && order.orderStatus === "Delivered"
+                        ? "greenColor"
+                        : "redColor"
+                    }
+                  >
+                    {order.orderStatus && order.orderStatus}
+                  </p>
+                </div>
+              </div>
 
             </div>
 
