2a9fc1bba6a401a8976372d45fce7a09908777a6
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-17 14:12:16 -0500

    add action for getting order details

--- frontend/src/actions/orderAction.js
+++ frontend/src/actions/orderAction.js
@@ -5,6 +5,10 @@ import {
   MY_ORDERS_REQUEST,
   MY_ORDERS_SUCCESS,
   MY_ORDERS_FAIL,
+
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_FAIL,
   
   CLEAR_ERRORS,
 } from "../constants/orderConstants";
@@ -48,6 +52,22 @@ export const myOrders = () => async (dispatch) => {
   }
 };
 
+
+// Get Order Details
+export const getOrderDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: ORDER_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/v1/order/${id}`);
+
+    dispatch({ type: ORDER_DETAILS_SUCCESS, payload: data.order });
+  } catch (error) {
+    dispatch({
+      type: ORDER_DETAILS_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
 // Clearing Errors
 export const clearErrors = () => async (dispatch) => {
