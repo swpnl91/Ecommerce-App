5f0bec0b2ae27a908f1876b831188ef00eddc3bc
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-17 14:18:13 -0500

    create OrderDetails.js

---
+++ frontend/src/component/Order/OrderDetails.js
@@ -0,0 +1,38 @@
+import React, { Fragment, useEffect } from "react";
+import "./OrderDetails.css";
+
+
+const OrderDetails = ({ match }) => {
+  
+  
+  
+  return (
+    <Fragment>
+      
+
+        <Fragment>
+
+          
+
+          <div className="orderDetailsPage">
+            
+            <div className="orderDetailsContainer">
+              
+              
+
+            </div>
+
+            <div className="orderDetailsCartItems">
+              
+              
+            </div>
+
+          </div>
+
+        </Fragment>
+     
+    </Fragment>
+  );
+};
+
+export default OrderDetails;
