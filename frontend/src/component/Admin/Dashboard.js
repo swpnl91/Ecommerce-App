1f681b687b2f825910bf7aee7925a2facaa3fc86
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-17 16:23:00 -0500

    create  Dashboard.js

---
+++ frontend/src/component/Admin/Dashboard.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+import "./Dashboard.css";
+
+
+const Dashboard = () => {
+  
+  
+
+  return (
+    <div className="dashboard">
+      
+
+      <div className="dashboardContainer">
+        
+      </div>
+      
+    </div>
+  );
+};
+
+export default Dashboard;
