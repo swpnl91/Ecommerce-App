a522329cf58100e05687f0162c6397c7ad70391b
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-21 13:59:22 -0500

    create ProductList.js

---
+++ frontend/src/component/Admin/ProductList.js
@@ -0,0 +1,20 @@
+import React, { Fragment, useEffect } from "react";
+import "./ProductList.css";
+
+
+const ProductList = ({ history }) => {
+  
+
+  return (
+    <Fragment>
+      
+
+      <div className="dashboard">
+        
+      </div>
+      
+    </Fragment>
+  );
+};
+
+export default ProductList;
