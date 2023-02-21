5e82e73295855e4b071c99663e84d8fad65876e1
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-21 15:51:58 -0500

    create NewProduct.js

---
+++ frontend/src/component/Admin/NewProduct.js
@@ -0,0 +1,18 @@
+import React, { Fragment, useEffect, useState } from "react";
+import "./nNewProduct.css";
+
+
+const NewProduct = ({ history }) => {
+  
+
+  return (
+    <Fragment>
+      
+      <div className="dashboard">
+        
+      </div>
+    </Fragment>
+  );
+};
+
+export default NewProduct;
