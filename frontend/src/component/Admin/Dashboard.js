8b8d64bc4e62f7c6bd98c1cd9cbb897250512917
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-21 13:34:43 -0500

    create lineState and doughnutState in dashboard.js

--- frontend/src/component/Admin/Dashboard.js
+++ frontend/src/component/Admin/Dashboard.js
@@ -41,6 +41,29 @@ const Dashboard = () => {
     orders.forEach((item) => {
       totalAmount += item.totalPrice;
     });
+
+  const lineState = {
+    labels: ["Initial Amount", "Amount Earned"],
+    datasets: [
+      {
+        label: "TOTAL AMOUNT",
+        backgroundColor: ["tomato"],
+        hoverBackgroundColor: ["rgb(197, 72, 49)"],
+        data: [0, totalAmount],
+      },
+    ],
+  };
+  
+  const doughnutState = {
+    labels: ["Out of Stock", "InStock"],
+    datasets: [
+      {
+        backgroundColor: ["#00A6B4", "#6800B4"],
+        hoverBackgroundColor: ["#4B5000", "#35014F"],
+        data: [outOfStock, products.length - outOfStock],
+      },
+    ],
+  };
   
 
   return (
