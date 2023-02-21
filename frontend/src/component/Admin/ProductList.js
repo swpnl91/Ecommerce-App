0fc01ecdfadfca016cff67097932884941db4628
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-21 14:26:53 -0500

    add logic and fucntionality for ProductList.js

--- frontend/src/component/Admin/ProductList.js
+++ frontend/src/component/Admin/ProductList.js
@@ -19,6 +19,104 @@ import { DELETE_PRODUCT_RESET } from "../../constants/productConstants";
 
 const ProductList = ({ history }) => {
   
+  const dispatch = useDispatch();
+
+  const alert = useAlert();
+
+  const { error, products } = useSelector((state) => state.products);
+
+  const { error: deleteError, isDeleted } = useSelector(
+    (state) => state.product
+  );
+
+  const deleteProductHandler = (id) => {
+    dispatch(deleteProduct(id));
+  };
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+
+    if (deleteError) {
+      alert.error(deleteError);
+      dispatch(clearErrors());
+    }
+
+    if (isDeleted) {
+      alert.success("Product Deleted Successfully");
+      history.push("/admin/dashboard");
+      dispatch({ type: DELETE_PRODUCT_RESET });
+    }
+
+    dispatch(getAdminProduct());
+  }, [dispatch, alert, error, deleteError, history, isDeleted]);
+
+  const columns = [
+    { field: "id", headerName: "Product ID", minWidth: 200, flex: 0.5 },
+
+    {
+      field: "name",
+      headerName: "Name",
+      minWidth: 350,
+      flex: 1,
+    },
+    {
+      field: "stock",
+      headerName: "Stock",
+      type: "number",
+      minWidth: 150,
+      flex: 0.3,
+    },
+
+    {
+      field: "price",
+      headerName: "Price",
+      type: "number",
+      minWidth: 270,
+      flex: 0.5,
+    },
+
+    {
+      field: "actions",
+      flex: 0.3,
+      headerName: "Actions",
+      minWidth: 150,
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <Fragment>
+            <Link to={`/admin/product/${params.getValue(params.id, "id")}`}>
+              <EditIcon />
+            </Link>
+
+            <Button
+              onClick={() =>
+                deleteProductHandler(params.getValue(params.id, "id"))
+              }
+            >
+              <DeleteIcon />
+            </Button>
+          </Fragment>
+        );
+      },
+    },
+  ];
+
+  const rows = [];
+
+  products &&
+    products.forEach((item) => {
+      rows.push({
+        id: item._id,
+        stock: item.Stock,
+        price: item.price,
+        name: item.name,
+      });
+    });
+
 
   return (
     <Fragment>
