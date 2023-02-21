a7c972f015bbba7ff4ee104cd6cca85c7839c8b8
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-21 14:25:31 -0500

    import required packages and files  for ProductList.js

--- frontend/src/component/Admin/ProductList.js
+++ frontend/src/component/Admin/ProductList.js
@@ -1,5 +1,20 @@
 import React, { Fragment, useEffect } from "react";
 import "./ProductList.css";
+import { DataGrid } from "@material-ui/data-grid";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  clearErrors,
+  getAdminProduct,
+  deleteProduct,
+} from "../../actions/productAction";
+import { Link } from "react-router-dom";
+import { useAlert } from "react-alert";
+import { Button } from "@material-ui/core";
+import MetaData from "../layout/MetaData";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import SideBar from "./Sidebar";
+import { DELETE_PRODUCT_RESET } from "../../constants/productConstants";
 
 
 const ProductList = ({ history }) => {
