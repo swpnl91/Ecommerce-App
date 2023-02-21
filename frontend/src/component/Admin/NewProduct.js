9eb14acee87397dc2f25e980095d77e56378ba89
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-21 15:55:02 -0500

    import required files/packages in NewProduct.js

--- frontend/src/component/Admin/NewProduct.js
+++ frontend/src/component/Admin/NewProduct.js
@@ -1,5 +1,17 @@
 import React, { Fragment, useEffect, useState } from "react";
-import "./nNewProduct.css";
+import "./NewProduct.css";
+import { useSelector, useDispatch } from "react-redux";
+import { clearErrors, createProduct } from "../../actions/productAction";
+import { useAlert } from "react-alert";
+import { Button } from "@material-ui/core";
+import MetaData from "../layout/MetaData";
+import AccountTreeIcon from "@material-ui/icons/AccountTree";
+import DescriptionIcon from "@material-ui/icons/Description";
+import StorageIcon from "@material-ui/icons/Storage";
+import SpellcheckIcon from "@material-ui/icons/Spellcheck";
+import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
+import SideBar from "./Sidebar";
+import { NEW_PRODUCT_RESET } from "../../constants/productConstants";
 
 
 const NewProduct = ({ history }) => {
