84dd6f74cccfec5fdcf1106dd12cd1404158fa22
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-22 13:29:36 -0500

    add reducer for editing an item/product as an admin

--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -9,6 +9,10 @@ import {
   NEW_PRODUCT_SUCCESS,
   NEW_PRODUCT_FAIL,
   NEW_PRODUCT_RESET,
+  UPDATE_PRODUCT_REQUEST,
+  UPDATE_PRODUCT_SUCCESS,
+  UPDATE_PRODUCT_FAIL,
+  UPDATE_PRODUCT_RESET,
   DELETE_PRODUCT_REQUEST,
   DELETE_PRODUCT_SUCCESS,
   DELETE_PRODUCT_FAIL,
@@ -108,7 +112,7 @@ export const productReducer = (state = {}, action) => {
   switch (action.type) {
     
     case DELETE_PRODUCT_REQUEST:
-    
+    case UPDATE_PRODUCT_REQUEST:
       return {
         ...state,
         loading: true,
@@ -121,9 +125,16 @@ export const productReducer = (state = {}, action) => {
         isDeleted: action.payload,
       };
 
+    case UPDATE_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isUpdated: action.payload,
+      };
+
     
     case DELETE_PRODUCT_FAIL:
-    
+    case UPDATE_PRODUCT_FAIL:
       return {
         ...state,
         loading: false,
@@ -136,7 +147,12 @@ export const productReducer = (state = {}, action) => {
         isDeleted: false,
       };
 
-    
+    case UPDATE_PRODUCT_RESET:
+      return {
+        ...state,
+        isUpdated: false,
+      };
+
     case CLEAR_ERRORS:
       return {
         ...state,
