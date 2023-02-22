9be47b50009e459ea03ac58e9ae011a1414ec431
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-22 16:13:14 -0500

    create ProcessOrder.js for admin to update order

---
+++ frontend/src/component/Admin/ProcessOrder.js
@@ -0,0 +1,20 @@
+import React, { Fragment, useEffect, useState } from "react";
+import "./ProcessOrder.css";
+
+const ProcessOrder = ({ history, match }) => {
+  
+
+  return (
+    <Fragment>
+      
+
+
+      <div className="dashboard">
+        
+      </div>
+
+    </Fragment>
+  );
+};
+
+export default ProcessOrder;
