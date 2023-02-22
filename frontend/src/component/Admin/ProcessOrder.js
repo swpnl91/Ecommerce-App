a8889eb3f24b2a5c5d5833077e3ce54f769b56b7
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-22 16:14:06 -0500

    import required files/packages in ProcessOrder.js for admin to update order

--- frontend/src/component/Admin/ProcessOrder.js
+++ frontend/src/component/Admin/ProcessOrder.js
@@ -1,5 +1,22 @@
 import React, { Fragment, useEffect, useState } from "react";
 import "./ProcessOrder.css";
+import MetaData from "../layout/MetaData";
+import { Link } from "react-router-dom";
+import { Typography } from "@material-ui/core";
+import SideBar from "./Sidebar";
+import {
+  getOrderDetails,
+  clearErrors,
+  updateOrder,
+} from "../../actions/orderAction";
+import { useSelector, useDispatch } from "react-redux";
+import Loader from "../layout/Loader/Loader";
+import { useAlert } from "react-alert";
+import AccountTreeIcon from "@material-ui/icons/AccountTree";
+import { Button } from "@material-ui/core";
+import { UPDATE_ORDER_RESET } from "../../constants/orderConstants";
+
+
 
 const ProcessOrder = ({ history, match }) => {
   
