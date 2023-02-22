66a02c83c9548d5a399bde6990cd7c95cd6cee24
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-22 13:39:02 -0500

    import required files/packages for UpdateProduct.js

--- frontend/src/component/Admin/UpdateProduct.js
+++ frontend/src/component/Admin/UpdateProduct.js
@@ -1,4 +1,20 @@
 import React, { Fragment, useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  clearErrors,
+  updateProduct,
+  getProductDetails,
+} from "../../actions/productAction";
+import { useAlert } from "react-alert";
+import { Button } from "@material-ui/core";
+import MetaData from "../layout/MetaData";
+import AccountTreeIcon from "@material-ui/icons/AccountTree";
+import DescriptionIcon from "@material-ui/icons/Description";
+import StorageIcon from "@material-ui/icons/Storage";
+import SpellcheckIcon from "@material-ui/icons/Spellcheck";
+import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
+import SideBar from "./Sidebar";
+import { UPDATE_PRODUCT_RESET } from "../../constants/productConstants";
 
 
 const UpdateProduct = ({ history, match }) => {
