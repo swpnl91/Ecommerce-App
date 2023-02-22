ee5aff70860fe5056d9c73ebd921050f369a3aa2
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-22 14:21:25 -0500

    add OrderList.js for accessing all orders as an admin

---
+++ frontend/src/component/Admin/OrderList.js
@@ -0,0 +1,21 @@
+import React, { Fragment, useEffect } from "react";
+
+
+
+
+const OrderList = ({ history }) => {
+  
+
+  return (
+    <Fragment>
+      
+
+      <div className="dashboard">
+        
+      </div>
+
+    </Fragment>
+  );
+};
+
+export default OrderList;
