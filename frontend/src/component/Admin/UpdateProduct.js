33c255e81da92c90838353409f1701587bc6f55a
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-22 13:41:23 -0500

    add structure to UpdateProduct.js

--- frontend/src/component/Admin/UpdateProduct.js
+++ frontend/src/component/Admin/UpdateProduct.js
@@ -27,6 +27,109 @@ const UpdateProduct = ({ history, match }) => {
       
       <div className="dashboard">
         
+        <SideBar />
+
+        <div className="newProductContainer">
+          <form
+            className="createProductForm"
+            encType="multipart/form-data"
+            onSubmit={updateProductSubmitHandler}
+          >
+            <h1>Create Product</h1>
+
+            <div>
+              <SpellcheckIcon />
+              <input
+                type="text"
+                placeholder="Product Name"
+                required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            
+            <div>
+              <AttachMoneyIcon />
+              <input
+                type="number"
+                placeholder="Price"
+                required
+                onChange={(e) => setPrice(e.target.value)}
+                value={price}
+              />
+            </div>
+
+            <div>
+              <DescriptionIcon />
+
+              <textarea
+                placeholder="Product Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                cols="30"
+                rows="1"
+              ></textarea>
+            </div>
+
+            <div>
+              <AccountTreeIcon />
+              <select
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                <option value="">Choose Category</option>
+                {categories.map((cate) => (
+                  <option key={cate} value={cate}>
+                    {cate}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div>
+              <StorageIcon />
+              <input
+                type="number"
+                placeholder="Stock"
+                required
+                onChange={(e) => setStock(e.target.value)}
+                value={Stock}
+              />
+            </div>
+
+            <div id="createProductFormFile">
+              <input
+                type="file"
+                name="avatar"
+                accept="image/*"
+                onChange={updateProductImagesChange}
+                multiple
+              />
+            </div>
+
+            <div id="createProductFormImage">
+              {oldImages &&
+                oldImages.map((image, index) => (
+                  <img key={index} src={image.url} alt="Old Product Preview" />
+                ))}
+            </div>
+
+            <div id="createProductFormImage">
+              {imagesPreview.map((image, index) => (
+                <img key={index} src={image} alt="Product Preview" />
+              ))}
+            </div>
+
+            <Button
+              id="createProductBtn"
+              type="submit"
+              disabled={loading ? true : false}
+            >
+              Create
+            </Button>
+          </form>
+        </div>
+
       </div>
 
     </Fragment>
