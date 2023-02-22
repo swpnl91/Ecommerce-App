668d0c4edec93c24b1664d2895b119c636995534
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-22 13:38:02 -0500

    create UpdateProduct.js

---
+++ frontend/src/component/Admin/UpdateProduct.js
@@ -0,0 +1,20 @@
+import React, { Fragment, useEffect, useState } from "react";
+
+
+const UpdateProduct = ({ history, match }) => {
+  
+
+  return (
+    <Fragment>
+      
+      
+      
+      <div className="dashboard">
+        
+      </div>
+
+    </Fragment>
+  );
+};
+
+export default UpdateProduct;
