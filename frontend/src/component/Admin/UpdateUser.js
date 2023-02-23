0993a1f37d5e811d12cf887953bceacf4eed55d0
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-23 16:55:33 -0500

    add structure for UpdateUser.js

--- frontend/src/component/Admin/UpdateUser.js
+++ frontend/src/component/Admin/UpdateUser.js
@@ -26,6 +26,62 @@ const UpdateUser = ({ history, match }) => {
       
       <div className="dashboard">
         
+      <SideBar />
+        <div className="newProductContainer">
+          
+
+
+            <form
+              className="createProductForm"
+              onSubmit={updateUserSubmitHandler}
+            >
+              <h1>Update User</h1>
+
+              <div>
+                <PersonIcon />
+                <input
+                  type="text"
+                  placeholder="Name"
+                  required
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              
+              <div>
+                <MailOutlineIcon />
+                <input
+                  type="email"
+                  placeholder="Email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <VerifiedUserIcon />
+                <select value={role} onChange={(e) => setRole(e.target.value)}>
+                  <option value="">Choose Role</option>
+                  <option value="admin">Admin</option>
+                  <option value="user">User</option>
+                </select>
+              </div>
+
+              <Button
+                id="createProductBtn"
+                type="submit"
+                disabled={
+                  updateLoading ? true : false || role === "" ? true : false
+                }
+              >
+                Update
+              </Button>
+            </form>
+          
+
+        </div>
+
       </div>
 
     </Fragment>
