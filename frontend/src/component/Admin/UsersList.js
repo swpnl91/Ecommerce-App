7cce0c9ff4b0b133684afe93593426fa22417fd1
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-23 16:24:06 -0500

    import required packages/files in UserList.js

--- frontend/src/component/Admin/UsersList.js
+++ frontend/src/component/Admin/UsersList.js
@@ -1,5 +1,16 @@
 import React, { Fragment, useEffect } from "react";
 import "./ProductList.css";
+import { DataGrid } from "@material-ui/data-grid";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { useAlert } from "react-alert";
+import { Button } from "@material-ui/core";
+import MetaData from "../layout/MetaData";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import SideBar from "./Sidebar";
+import { getAllUsers, clearErrors, deleteUser } from "../../actions/userAction";
+import { DELETE_USER_RESET } from "../../constants/userConstants";
 
 
 
