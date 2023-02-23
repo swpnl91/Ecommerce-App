903b0798a69a28706c019fc2a3f0b2698155ee37
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-23 16:24:59 -0500

    add structure to UserList.js

--- frontend/src/component/Admin/UsersList.js
+++ frontend/src/component/Admin/UsersList.js
@@ -24,6 +24,20 @@ const UsersList = ({ history }) => {
 
       <div className="dashboard">
         
+        <SideBar />
+        <div className="productListContainer">
+          <h1 id="productListHeading">ALL USERS</h1>
+
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={10}
+            disableSelectionOnClick
+            className="productListTable"
+            autoHeight
+          />
+        </div>
+
       </div>
 
     </Fragment>
