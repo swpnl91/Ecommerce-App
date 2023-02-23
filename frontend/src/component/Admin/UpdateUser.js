f24426bc82e102fb14d422c14fa2c82e02f43702
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-23 16:52:01 -0500

    create UpdateUser

---
+++ frontend/src/component/Admin/UpdateUser.js
@@ -0,0 +1,20 @@
+import React, { Fragment, useEffect, useState } from "react";
+
+
+const UpdateUser = ({ history, match }) => {
+  
+
+  return (
+    <Fragment>
+      
+      
+      
+      <div className="dashboard">
+        
+      </div>
+
+    </Fragment>
+  );
+};
+
+export default UpdateUser;
