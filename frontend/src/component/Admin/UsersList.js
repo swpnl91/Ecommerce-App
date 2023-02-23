518b5ab7c06fd13e7a316b59b7d8f80e25d3d65e
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-23 16:23:16 -0500

    create UserList.js

---
+++ frontend/src/component/Admin/UsersList.js
@@ -0,0 +1,22 @@
+import React, { Fragment, useEffect } from "react";
+import "./ProductList.css";
+
+
+
+
+const UsersList = ({ history }) => {
+  
+
+  return (
+    <Fragment>
+      
+
+      <div className="dashboard">
+        
+      </div>
+
+    </Fragment>
+  );
+};
+
+export default UsersList;
