a0ecda5db70d53f36bf6a6abcc8b4d0f52b64027
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-23 16:29:42 -0500

    add logic for creating columns and rows in UserList.js

--- frontend/src/component/Admin/UsersList.js
+++ frontend/src/component/Admin/UsersList.js
@@ -53,6 +53,75 @@ const UsersList = ({ history }) => {
     dispatch(getAllUsers());
   }, [dispatch, alert, error, deleteError, history, isDeleted, message]);
 
+  const columns = [
+    { field: "id", headerName: "User ID", minWidth: 180, flex: 0.8 },
+
+    {
+      field: "email",
+      headerName: "Email",
+      minWidth: 200,
+      flex: 1,
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      minWidth: 150,
+      flex: 0.5,
+    },
+
+    {
+      field: "role",
+      headerName: "Role",
+      type: "number",
+      minWidth: 150,
+      flex: 0.3,
+      cellClassName: (params) => {
+        return params.getValue(params.id, "role") === "admin"
+          ? "greenColor"
+          : "redColor";
+      },
+    },
+
+    {
+      field: "actions",
+      flex: 0.3,
+      headerName: "Actions",
+      minWidth: 150,
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <Fragment>
+            <Link to={`/admin/user/${params.getValue(params.id, "id")}`}>
+              <EditIcon />
+            </Link>
+
+            <Button
+              onClick={() =>
+                deleteUserHandler(params.getValue(params.id, "id"))
+              }
+            >
+              <DeleteIcon />
+            </Button>
+          </Fragment>
+        );
+      },
+    },
+  ];
+
+  const rows = [];
+
+  users &&
+    users.forEach((item) => {
+      rows.push({
+        id: item._id,
+        role: item.role,
+        email: item.email,
+        name: item.name,
+      });
+    });
+    
+
   return (
     <Fragment>
       
