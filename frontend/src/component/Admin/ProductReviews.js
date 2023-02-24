e40e36bedf168b7c32930641827cf554663a73e0
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-24 15:10:55 -0500

    import required files/packages for ProductReviews.js

--- frontend/src/component/Admin/ProductReviews.js
+++ frontend/src/component/Admin/ProductReviews.js
@@ -1,5 +1,21 @@
 import React, { Fragment, useEffect, useState } from "react";
 import "./ProductReviews.css";
+import { DataGrid } from "@material-ui/data-grid";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  clearErrors,
+  getAllReviews,
+  deleteReviews,
+} from "../../actions/productAction";
+import { useAlert } from "react-alert";
+import { Button } from "@material-ui/core";
+import MetaData from "../layout/MetaData";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Star from "@material-ui/icons/Star";
+
+import SideBar from "./Sidebar";
+import { DELETE_REVIEW_RESET } from "../../constants/productConstants";
+
 
 
 const ProductReviews = ({ history }) => {
