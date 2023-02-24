8398671d17a62bbfd19eaf5feb70e12b96503a53
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-24 15:20:51 -0500

    add logic for creating DataGrid rows and columns in ProductReviews.js

--- frontend/src/component/Admin/ProductReviews.js
+++ frontend/src/component/Admin/ProductReviews.js
@@ -63,6 +63,72 @@ const ProductReviews = ({ history }) => {
       dispatch({ type: DELETE_REVIEW_RESET });
     }
   }, [dispatch, alert, error, deleteError, history, isDeleted, productId]);
+
+  const columns = [
+    { field: "id", headerName: "Review ID", minWidth: 200, flex: 0.5 },
+
+    {
+      field: "user",
+      headerName: "User",
+      minWidth: 200,
+      flex: 0.6,
+    },
+
+    {
+      field: "comment",
+      headerName: "Comment",
+      minWidth: 350,
+      flex: 1,
+    },
+
+    {
+      field: "rating",
+      headerName: "Rating",
+      type: "number",
+      minWidth: 180,
+      flex: 0.4,
+
+      cellClassName: (params) => {
+        return params.getValue(params.id, "rating") >= 3
+          ? "greenColor"
+          : "redColor";
+      },
+    },
+
+    {
+      field: "actions",
+      flex: 0.3,
+      headerName: "Actions",
+      minWidth: 150,
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <Fragment>
+            <Button
+              onClick={() =>
+                deleteReviewHandler(params.getValue(params.id, "id"))
+              }
+            >
+              <DeleteIcon />
+            </Button>
+          </Fragment>
+        );
+      },
+    },
+  ];
+
+  const rows = [];
+
+  reviews &&
+    reviews.forEach((item) => {
+      rows.push({
+        id: item._id,
+        rating: item.rating,
+        comment: item.comment,
+        user: item.name,
+      });
+    });
   
 
   return (
