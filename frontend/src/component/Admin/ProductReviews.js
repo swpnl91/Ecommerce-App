4ec1d20f2e0a34a0bcc7579e6cf8b414738e1124
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-02-24 14:51:43 -0500

    create ProductReviews.js

---
+++ frontend/src/component/Admin/ProductReviews.js
@@ -0,0 +1,24 @@
+import React, { Fragment, useEffect, useState } from "react";
+import "./ProductReviews.css";
+
+
+const ProductReviews = ({ history }) => {
+  
+    
+
+  return (
+    <Fragment>
+      
+
+
+      <div className="dashboard">
+        
+        
+
+      </div>
+
+    </Fragment>
+  );
+};
+
+export default ProductReviews;
